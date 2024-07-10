033fd77759a5858eba1664e5ea0283e25437d226
Ashley <ashedencv@gmail.com>
2024-07-09 21:03:36 -0600

    Adicion de la funcionalidad para desencriptar con texto

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function encriptar(){
     let texto = document.getElementById("texto").value;
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
+    const muneco = document.getElementById("muneco");
+    let botonCopiar = document.getElementById("botonCopiar");
 
     let textoCifrado = texto
                         .replace(/e/gi, "enter")
@@ -10,9 +12,37 @@ function encriptar(){
                         .replace(/o/gi, "ober")
                         .replace(/u/gi, "ufat");
     if (document.getElementById("texto").value.length != 0) {
-        document.getElementById("texto").value = textoCifrado
-        document.getElementById("titulo-mensaje").textContent = "Texto encriptado con exito";
+        document.getElementById("texto").value = "";
+        document.getElementById("titulo-mensaje").textContent = textoCifrado;
         document.getElementById("parrafo").textContent = "";
+        muneco.style.display = "none";
+        botonCopiar.style.display = "block";
+    } else{
+        alert ("Debes ingresas algun texto");
     }
-        
+}
+
+function desencriptar (){
+    let texto = document.getElementById("texto").value;
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = "";
+        document.getElementById("titulo-mensaje").textContent = textoCifrado;
+        document.getElementById("parrafo").textContent = "";
+    }else{
+        alert ("Debes ingresas algun texto");
+    }
+}
+
+botonCopiar.addEventListener ("click"), () => {
+    let texto = textoCifrado;
+    texto.select();
+    document.execCommand('copy')
 }
